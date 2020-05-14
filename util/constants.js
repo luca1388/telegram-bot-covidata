d212bafb78431cea147c6acc49441c6d3306b671
Luca Gandini <luca.gandini88@gmail.com>
2020-05-14 17:51:08 +0200

    fix: Added telegram token

--- util/constants.js
+++ util/constants.js
@@ -1,3 +1,3 @@
 exports.telegramApiBaseUrl = "https://api.telegram.org/bot";
-exports.telegramApiToken = "your-private-token";
+exports.telegramApiToken = "990730454:AAFSka3uB86j_cRbc9B80Qs41oCOiTaz8Hc";
 exports.telegramSendMessagePath = 'sendMessage';
