270ef7821e878f43bd45b9fc654f692e326cad36
Luca Gandini <luca.gandini88@gmail.com>
2020-05-15 15:58:54 +0200

    Read telegram token from env var

--- util/constants.js
+++ util/constants.js
@@ -1,3 +1,3 @@
 exports.telegramApiBaseUrl = "https://api.telegram.org/bot";
-exports.telegramApiToken = "990730454:AAFSka3uB86j_cRbc9B80Qs41oCOiTaz8Hc";
+exports.telegramApiToken = process.env.TELEGRAM_TOKEN;
 exports.telegramSendMessagePath = 'sendMessage';
